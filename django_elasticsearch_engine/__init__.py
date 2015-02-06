c837478133f4c1255e0beff470a4c4456333fc17
Jorge Alegre <jalegre39@gmail.com>
2015-02-06 21:13:06 +0100

    styles

--- django_elasticsearch_engine/__init__.py
+++ django_elasticsearch_engine/__init__.py
@@ -2,7 +2,8 @@ import django.db.models.options as options
 
 __author__ = 'jorgealegre'
 
-options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('indices', 'disable_default_index')
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('indices',
+                                                 'disable_default_index')
 
 
 class DjangoElasticEngineException(Exception):
