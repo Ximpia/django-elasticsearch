fc3bdaae9b872354d8893ce23a142af9aa633d7d
Jorge Alegre <jalegre39@gmail.com>
2015-02-09 00:50:12 +0100

    consider only default connection

--- django_elasticsearch_engine/management/commands/migrate.py
+++ django_elasticsearch_engine/management/commands/migrate.py
@@ -1,11 +1,11 @@
 # python
 import logging
 import pprint
+import sys
 
 # django
 from django.conf import settings
-# from django.core.management.commands import syncdb
-from django.db import connections
+from django.db import connections, DEFAULT_DB_ALIAS
 from django.core.management.base import BaseCommand
 
 # pyes
@@ -30,43 +30,40 @@ class Command(BaseCommand):
         })
 
     def handle(self, *args, **options):
-        databases = settings.DATABASES.keys()
-        for db in databases:
-            engine = settings.DATABASES.get(db, {}).get('ENGINE', '')
-            index_name = settings.DATABASES.get(db, {}).get('NAME', '')
-            options = settings.DATABASES.get(db, {}).get('OPTIONS', {})
-            logger.debug(u'db: {} engine: {} index_name: {}'.format(db, engine, index_name))
+        engine = settings.DATABASES.get(DEFAULT_DB_ALIAS, {}).get('ENGINE', '')
+        index_name = settings.DATABASES.get(DEFAULT_DB_ALIAS, {}).get('NAME', '')
+        options = settings.DATABASES.get(DEFAULT_DB_ALIAS, {}).get('OPTIONS', {})
 
-            # Call regular migrate if engine is different from ours
-            if engine != ENGINE:
-                return super(Command, self).handle(**options)
-            else:
-                connection = connections[db]
-                es_connection = connection.connection
-                try:
-                    logger.debug(u'indices: {}'.format(es_connection.indices.get_indices()))
-                except IndexMissingException:
-                    pass
-                try:
+        # Call regular migrate if engine is different from ours
+        if engine != ENGINE:
+            return super(Command, self).handle(**options)
+        else:
+            connection = connections[DEFAULT_DB_ALIAS]
+            es_connection = connection.connection
+            try:
+                logger.info(u'indices: {}'.format(es_connection.indices.get_indices()))
+            except IndexMissingException:
+                pass
+            try:
+                self._create_index(es_connection, index_name, options)
+                self.stdout.write(u'Created index "{}"'.format(index_name))
+            except IndexAlreadyExistsException:
+                self.stderr.write(u'Index "{}" already exists!!!'.format(index_name))
+                ans = raw_input(u'\n** WARNING **\nI will remove index "{}". Do you want to continue? (y/n) <y> '
+                                .format(index_name))
+                ans = ans or 'y'
+                if ans.lower() == 'n':
+                    sys.exit(1)
+                else:
+                    es_connection.indices.delete_index(index_name)
+                    self.stdout.write(u'\nindex "{}" removed'.format(index_name))
                     self._create_index(es_connection, index_name, options)
-                    self.stdout.write(u'Created index "{}"'.format(index_name))
-                except IndexAlreadyExistsException:
-                    self.stderr.write(u'Index "{}" already exists!!!'.format(index_name))
-                    ans = raw_input(u'\n** WARNING **\nI will remove index "{}". Do you want to continue? (y/n) <y> '
-                                    .format(index_name))
-                    ans = ans or 'y'
-                    if ans.lower() == 'n':
-                        continue
-                    else:
-                        es_connection.indices.delete_index(index_name)
-                        self.stdout.write(u'\nindex "{}" removed'.format(index_name))
-                        self._create_index(es_connection, index_name, options)
-                        self.stdout.write(u'index "{}" created'.format(index_name))
-                logger.debug(u'models: {}'.format(connection.introspection.models))
-                for app_name, app_models in connection.introspection.models.iteritems():
-                    for model in app_models:
-                        self.stdout.write(u'Mappings %s.%s' % (app_name, model.__name__))
-                        mapping = model_to_mapping(model, es_connection, index_name)
-                        logger.debug(u'mapping: {}'.format(pprint.PrettyPrinter(indent=4).pformat(mapping)))
-                        logger.debug(u'mapping: {}'.format(pprint.PrettyPrinter(indent=4).pformat(mapping.as_dict())))
-                        mapping.save()
+                    self.stdout.write(u'index "{}" created'.format(index_name))
+            logger.debug(u'models: {}'.format(connection.introspection.models))
+            for app_name, app_models in connection.introspection.models.iteritems():
+                for model in app_models:
+                    self.stdout.write(u'Mappings %s.%s' % (app_name, model.__name__))
+                    mapping = model_to_mapping(model, es_connection, index_name)
+                    logger.debug(u'mapping: {}'.format(pprint.PrettyPrinter(indent=4).pformat(mapping)))
+                    logger.debug(u'mapping: {}'.format(pprint.PrettyPrinter(indent=4).pformat(mapping.as_dict())))
+                    mapping.save()
